9c5b32c5f22f462e79cc4581628fbbcf1a9fa4a2
Daniel Nagy <danielnagy@posteo.de>
2023-04-15 13:00:00 +0200

    wiktionary update

--- pkgs/servers/dict/wiktionary/update.sh
+++ pkgs/servers/dict/wiktionary/update.sh
@@ -1,7 +1,10 @@
 #! /usr/bin/env nix-shell
-#! nix-shell -i bash -p common-updater-scripts python3
+#! nix-shell -i bash -p nix-update xmlstarlet curl
 
 set -ueo pipefail
 
-version="$(python "$(dirname "${BASH_SOURCE[0]}")"/latest_version.py)"
-update-source-version dictdDBs.wiktionary "$version"
+RSSURL="https://dumps.wikimedia.org/enwiktionary/latest/enwiktionary-latest-pages-articles.xml.bz2-rss.xml"
+XPATH="substring-after(//item/link, 'http://download.wikimedia.org/enwiktionary/')"
+version=$(curl -fs "$RSSURL" | xml sel -t -v "$XPATH")
+( cd ../../../../
+  nix-update dictdDBs.wiktionary --version "$version" )
