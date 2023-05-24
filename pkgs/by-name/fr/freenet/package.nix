d6b890368100441ead39d7b9d6acd9e7af2f0792
Daniel Nagy <danielnagy@posteo.de>
2023-05-24 15:45:00 +0200

    freenet: add mainProgram

--- pkgs/by-name/fr/freenet/package.nix
+++ pkgs/by-name/fr/freenet/package.nix
@@ -126,5 +126,6 @@ stdenv.mkDerivation rec {
     maintainers = with lib.maintainers; [ nagy ];
     platforms = with lib.platforms; linux;
     changelog = "https://github.com/freenet/fred/blob/build${version}/NEWS.md";
+    mainProgram = "freenet";
   };
 }
